683ad19712a8798befe831368537d5be49f22c71
Katie Gorbell <katiegorbell@gmail.com>
2019-01-07 14:44:00 -0600

    less styles implemented, jumbotron initialized.

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Jumbotron from './Components/Jumbotron';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Jumbotron />
       </div>
     );
   }
---
+++ my-portfolio/src/Components/Jumbotron.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Jumbotron extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className='jumbotron'>
+
+            </div>
+        )
+    }
+}
+
+export default Jumbotron;
