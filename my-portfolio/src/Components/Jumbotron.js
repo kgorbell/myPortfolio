72371b920dc0131b83947b156193103d1bd37990
Katie Gorbell <katiegorbell@gmail.com>
2019-01-09 19:53:05 -0600

    images for jumbotron created and imported in

--- my-portfolio/src/Components/Jumbotron.js
+++ my-portfolio/src/Components/Jumbotron.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-
+//jumbotron images
+import Cloud1 from '../images/cloud1.png';
+import Cloud2 from '../images/cloud2.png';
+import Sun from '../images/sun.png';
+import Tree from '../images/tree.png';
+import Mtns_left from '../images/mountains_left.png';
+import Mtns_right from '../images/mountains_right.png';
 
 class Jumbotron extends Component {
     constructor() {
@@ -12,7 +18,13 @@ class Jumbotron extends Component {
     render() {
         return (
             <div className='jumbotron'>
-
+                {/* <Cloud1 /> */}
+                <img src={Cloud1} alt='cloud'/>
+                <img src={Cloud2} alt='cloud'/>
+                <img src={Sun} alt='sun'/>
+                <img src={Tree} alt='tree'/>
+                <img src={Mtns_left} alt='mountains'/>
+                <img src={Mtns_right} alt='mountains'/>
             </div>
         )
     }
